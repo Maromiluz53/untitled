f7b31e2877c77fc4ee6b09d8819eccb11d6b4fac
Maromiluz53 <maromiluz53@gmail.com>
2022-03-28 14:00:49 +0300

    This program can find out if you are a VIRGIN or a FUCKER

---
+++ Marom/Tirgulim.py
@@ -0,0 +1,11 @@
+num1=(input("enter your name :"))
+num2=int(input("enter your age :"))
+num3=int(input("enter your dick size :"))
+num4=int(input("how many girlfriends did you have ?"))
+num5=(input("what is your favorite position ?"))
+if num4>num3:
+    print("You are a FUCKER !")
+else:
+    if num3>num4:
+        print("You are a VIRGIN !")
+
